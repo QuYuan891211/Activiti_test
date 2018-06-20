ffdd6302ca9afc756bd0b32cbd6eeaac7d928405
quyuan <quyuan891211@163.com>
2018-06-20 21:31:17 +0800

    finish

--- src/main/java/cn/qy/activiti/TestActiviti2.java
+++ src/main/java/cn/qy/activiti/TestActiviti2.java
@@ -2,6 +2,13 @@ package cn.qy.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestActiviti2 {
     public ProcessEngine createProcessEngine(){
@@ -9,7 +16,29 @@ public class TestActiviti2 {
         return processEngineConfiguration.buildProcessEngine();
     }
 
+    @Test
     public void deployProcess(){
+        ProcessEngine processEngine = createProcessEngine();
+        Deployment deployment = processEngine.getRepositoryService().createDeployment().addClasspathResource("diagram/bankProcess.bpmn").addClasspathResource("diagram/bankProcess.png").name("bankProcess").category("bank").deploy();
 
+        System.out.println(deployment.getId()+ "   " + deployment.getName() + "   " + deployment.getDeploymentTime());
+    }
+
+    @Test
+    public void startProcess(){
+        ProcessEngine processEngine = createProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("bankProcess");
+        System.out.println(processInstance.getId()+ "   " + processInstance.getProcessDefinitionId());
+    }
+    @Test
+    public void complete(){
+        ProcessEngine processEngine = createProcessEngine();
+        Map<String,Object> params = new HashMap<String, Object>();
+
+        params.put("variable","express");
+        params.put("expressManager","qu");
+//        map_assginee.put()
+        processEngine.getTaskService().complete("4204",params);
     }
 }
